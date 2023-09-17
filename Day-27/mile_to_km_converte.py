28c149359b6a468d96fff14fdc117bf2812fc0d7
Jorge Nieto <93057254+georgen0101@users.noreply.github.com>
2023-09-16 22:33:44 -0600

    Day 29 - Pomodoro

--- Day-27/mile_to_km_converte.py
+++ Day-27/mile_to_km_converte.py
@@ -5,35 +5,49 @@ from tkinter import *
 screen = Tk()
 screen.title("Mile to Kilometer Converter")
 screen.config(padx=20, pady=20)
-# screen.minsize(width=500, height=300)
 
 # Labels
-label = Label(text="Miles")  # miles_label
-label.grid(column=2, row=0)
+miles_label = Label(text="Miles")  # miles_label
 
-label2 = Label(text="Kilometers")  # kilometer_label
-label2.grid(column=2, row=1)
+km_label = Label(text="Kilometers")  # kilometer_label
 
-label3 = Label(text="is equal to")  # is_equal_label
-label3.grid(column=0, row=1)
+is_equal_label = Label(text="is equal to")  # is_equal_label
 
-result = Label(text="0")  # kilometer_result_label
-result.grid(column=1, row=1)
+result_label = Label(text="0")  # kilometer_result_label
 
-# Entry
+# Use the radio widget to convert miles to km and vice-versa.
+# Radiobutton
+# def radio_used():
+#     print(radio_state.get())
+#
+#
+# # Variable to hold on to which radio button value is checked.
+# radio_state = IntVar()
+# radiobutton1 = Radiobutton(text="Option1", value=1, variable=radio_state, command=radio_used)
+# radiobutton2 = Radiobutton(text="Option2", value=2, variable=radio_state, command=radio_used)
+# radiobutton1.pack()
+# radiobutton2.pack()
+
+
 entry = Entry(width=10)  # miles_input/entry
 entry.insert(END, string="0")
-entry.grid(column=1, row=0)
 
 
-def button_action():  # miles_to_km
+def miles_to_km():  # miles_to_km
     miles = float(entry.get()) * 1.609
-    result.config(text=round(miles, 4))
+    result_label.config(text=round(miles, 4))
 
 
 # Create a button
-button = Button(text="Calculate", command=button_action)  # calculate_button
-button.grid(column=1, row=2)
+calculate_button = Button(text="Calculate", command=miles_to_km)  # calculate_button
+
+# Grid
+miles_label.grid(column=2, row=0)
+km_label.grid(column=2, row=1)
+is_equal_label.grid(column=0, row=1)
+result_label.grid(column=1, row=1)
+entry.grid(column=1, row=0)
+calculate_button.grid(column=1, row=2)
 
 
 screen.mainloop()
